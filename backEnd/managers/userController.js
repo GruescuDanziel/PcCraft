389f7f7224669cec1fa971c68a1918408a07a93b
SebiTCR <sebitcr@localhost.localdomain>
2021-02-20 11:18:59 +0200

    Fixed the login system

--- backEnd/managers/userController.js
+++ backEnd/managers/userController.js
@@ -6,10 +6,11 @@ const usermdl = require('../database/models/user')
 
 router.get('/createAcc', (req, res)=>
 {
+    console.log(req.body.last_name)
     let usr = new usermdl
     ({
         first_name: req.body.first_name,
-        last_name: req.bofy.last_name,
+        last_name: req.body.last_name,
         email: req.body.email,
         password: req.body.password,
         birthdate: req.body.birthdate,
@@ -17,12 +18,12 @@ router.get('/createAcc', (req, res)=>
         phoneNumber: req.body.phoneNumber
     })
 
-
-
-    usr.save(err)
+    usr.save((err)=>
     {
         console.error(err);
-    }
+    })
+    
+    res.send('User Created Successfully')
 })
 
 

3889abddf674870a5e49ad52c69a3ac57a39149b
sebiTCR <Ssebi832@outlook.com>
2021-02-21 18:45:23 +0200

    Cleaned up the code a bit

--- backEnd/database/models/user.js
+++ backEnd/database/models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-const User = mongoose.model('User', 
+const User = mongoose.model('User',
 {
-    first_name: String,
-    last_name: String,
-    email: String,
-    password: String,
-    birthdate: Date,
-    location: Array,
-    phoneNumber: Number
+    first_name:   String,
+    last_name:    String,
+    email:        String,
+    password:     String,
+    birthdate:    Date,
+    location:     Array,
+    phoneNumber:  Number
 });
 
 module.exports = User
--- backEnd/managers/userController.js
+++ backEnd/managers/userController.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../database/database')
-const usermdl = require('../database/models/user')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
+const express     = require('express');
+const router      = express.Router();
+const db          = require('../database/database')
+const usermdl     = require('../database/models/user')
+const bodyParser  = require('body-parser')
+const bcrypt      = require('bcrypt')
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -14,13 +14,13 @@ router.post('/signup', urlencodedParser,(req, res)=>
       {
         let usr = new usermdl
         ({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            password: hashed,
-            birthdate: req.body.birthdate,
-            location: req.body.location,
-            phoneNumber: req.body.phoneNumber
+            first_name:   req.body.first_name,
+            last_name:    req.body.last_name,
+            email:        req.body.email,
+            password:     hashed,
+            birthdate:    req.body.birthdate,
+            location:     req.body.location,
+            phoneNumber:  req.body.phoneNumber
         })
 
         usr.save();
@@ -71,11 +71,11 @@ router.put('/updateUsr', (req, res)=>
         else {
           res.send("Data changed successfully")
 
-          dataRes.email = req.body.email ? req.body.email : dataRes.email
-          dataRes.password = req.body.password ? hashed : dataRes.password
-          dataRes.first_name = req.body.first_name ? req.body.first_name : dataRes.first_name
-          dataRes.last_name = req.body.last_name ? req.body.last_name : dataRes.last_name
-          dataRes.location = req.body.location ? req.body.location : dataRes.location
+          dataRes.email       = req.body.email      ? req.body.email      : dataRes.email
+          dataRes.password    = req.body.password   ? hashed              : dataRes.password
+          dataRes.first_name  = req.body.first_name ? req.body.first_name : dataRes.first_name
+          dataRes.last_name   = req.body.last_name  ? req.body.last_name  : dataRes.last_name
+          dataRes.location    = req.body.location   ? req.body.location   : dataRes.location
 
           dataRes.save((err)=>{console.log(err)})
         }
